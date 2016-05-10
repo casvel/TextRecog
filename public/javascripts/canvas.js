54088c9d079b5bd482806e2b6566c7cf899eb3da
David Castillo <casvel.d@gmail.com>
2016-05-10 14:30:13 -0500

    Working on letters

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,8 +1,10 @@
 $(document).ready(function() 
 {
 
-  var canvas = $('#canvas');
-  var offset = canvas.offset();
+  console.log("canvas: " + active_canvas);
+
+  var all_canvas = $('canvas');
+  var offset = $("#"+all_canvas[1].id).offset();
   var clearBtn = $('#clearBtn');
 
   var lineColor = '#333';
@@ -20,19 +22,21 @@ $(document).ready(function()
   };
 
   fillWhite();
+  initCanvas();
 
   /* To fill the canvas with white */
   function fillWhite()
   {
-    var plain_canvas = document.getElementById("canvas");
+    var plain_canvas = document.getElementById("canvas"+active_canvas);
     var ctx = plain_canvas.getContext('2d');
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, plain_canvas.width, plain_canvas.height);
   }
 
   //jCanvas drawLine() method
-  function paintLine(x1, y1, x2, y2, paintWidth, paintColor) 
+  function paintLine(canvas, x1, y1, x2, y2, paintWidth, paintColor) 
   {
+    console.log(x1, y1, x2, y2);
     canvas.drawLine({
       strokeStyle: paintColor,
       strokeWidth: paintWidth,
@@ -50,40 +54,48 @@ $(document).ready(function()
    ** PAINTING FUNCTIONALITY **
    */
 
-  //On mousedown the painting functionality kicks in
-  canvas.on('mousedown', function(e) 
+  function initCanvas()
   {
-    isMouseDown = true;
-  });
-
-  //On mouseup the painting functionality stops
-  canvas.on('mouseup', function() 
-  {
-    isMouseDown = false;
-    return;
-  });
-
-  //On mousemove store the mouse coordinates and 
-  //use jCanvas drawLine() method
-  canvas.on('mousemove', function(e) 
-  {
-
-    lastPos.x = pos.x;
-    lastPos.y = pos.y;
-    pos.x = e.pageX - offset.left;
-    pos.y = e.pageY - offset.top;
-
-    if (isMouseDown) 
+    for (var i = 0; i < 52; i++)
     {
-      paintLine(lastPos.x, lastPos.y, pos.x, pos.y, lineWidthVal, lineColor);
-    }
-  });
+      canvas = $("#"+all_canvas[i].id);
+      
+      //On mousedown the painting functionality kicks in
+      canvas.on('mousedown', function(e) 
+      {
+        isMouseDown = true;
+      });
 
-  //Clears all canvas surface
-  clearBtn.on('click', function()
-  {
-    canvas.clearCanvas();
-    fillWhite();
-  });
+      //On mouseup the painting functionality stops
+      canvas.on('mouseup', function() 
+      {
+        isMouseDown = false;
+        return;
+      });
+
+      //On mousemove store the mouse coordinates and 
+      //use jCanvas drawLine() method
+      canvas.on('mousemove', function(e) 
+      {
+
+        lastPos.x = pos.x;
+        lastPos.y = pos.y;
+        pos.x = e.pageX - offset.left;
+        pos.y = e.pageY - offset.top;
+
+        if (isMouseDown) 
+        {
+          paintLine($("#"+this.id), lastPos.x, lastPos.y, pos.x, pos.y, lineWidthVal, lineColor);
+        }
+      });
+    }
+
+    //Clears all canvas surface
+    clearBtn.on('click', function()
+    {
+      canvas.clearCanvas();
+      fillWhite();
+    });
+  }
 
 });
--- public/javascripts/letters.js
+++ public/javascripts/letters.js
@@ -1,6 +1,43 @@
 $(document).ready(function()
 {
 
+	console.log("letters: " + active_canvas);
+
+	letters = new Array(52);
+	for (var i = 0; i < 52; i += 2)
+	{
+		var x = i/2;
+		letters[i]   = x+65;
+		letters[i+1] = x+97; 
+	}
+	letters = shuffleArray(letters);
+
+	function shuffleArray(array) 
+	{
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+	}
+
+
+	$("#tLetter").html(String.fromCharCode(letters[0]));
+	$('#pag_letters').bootpag({
+	  total: 52,
+	  maxVisible: 10,
+	  leaps: false,
+	  firstLastUse: true
+	}).on("page", function(event, num)
+	{
+	  $("#tLetter").html(String.fromCharCode(letters[num-1]));
+	  $("#canvas-container"+active_canvas).css("display", "none");
+	  active_canvas = num-1;
+	  $("#canvas-container"+active_canvas).css("display", "inline");
+	});
+
 	$("#saveBtn").click(function()
 	{
 		var canvas  = document.getElementById("canvas");
